76f9f6473e4ffa25fafa901f66841bb3b06db9cd
Norskiy <112186856+NorskiyXVIII@users.noreply.github.com>
2025-01-22 21:54:48 +0700

    Update socket.c

--- kanistra/net/socket.c
+++ kanistra/net/socket.c
@@ -1,5 +1,14 @@
 #include "socket.h"
 
+size_t getIndex(const char* str, const char sym) {
+    int i = 0;
+    while (str[i] != sym) i++;
+
+    return i;
+}
+
+#define get(BUFFER) fgets(BUFFER, sizeof(BUFFER), stdin); \
+BUFFER[getIndex(BUFFER, '\n')] = '\0';
 
 void kansock_close(int sock) {
     #ifdef linux
